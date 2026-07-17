699587eb6df57202e5b45fa8e9087c246ea77340
quratulainimtiaz <quratulainimtiaz852@gmail.com>
2026-07-17 15:13:59 +0500

    string to number

--- file8.js
+++ file8.js
@@ -1,4 +1,6 @@
 let age = 22;
 let firstNamen = "quratulain";
 // console.log(typeof age);
-console.log(typeof (age + ""));
+// console.log(typeof (age + ""));
+let myStr = +"56";
+console.log(typeof myStr);
