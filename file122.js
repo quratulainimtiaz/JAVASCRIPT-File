27fb01105293cf23f68f52dad0a6bb65957928ab
quratulainimtiaz <quratulainimtiaz852@gmail.com>
2026-07-24 11:51:06 +0500

    Traversing DOM Tree

---
+++ file122.js
@@ -0,0 +1,7 @@
+// Traversing DOM Tree
+
+const parent = document.getElementById("parent");
+
+console.log(parent.children);
+console.log(parent.firstElementChild);
+console.log(parent.lastElementChild);
