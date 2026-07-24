93fdf03917b731a702639c4f1e81c68a73d1f3e2
quratulainimtiaz <quratulainimtiaz852@gmail.com>
2026-07-24 11:28:13 +0500

    textContent & innerText

---
+++ file115.js
@@ -0,0 +1,8 @@
+// textContent & innerText
+
+const heading = document.getElementById("heading");
+
+console.log(heading.textContent);
+console.log(heading.innerText);
+
+heading.textContent = "Welcome Annie";
