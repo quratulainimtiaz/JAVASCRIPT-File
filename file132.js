7bc0656b32dec23564cc06ac1243522918bf04bc
quratulainimtiaz <quratulainimtiaz852@gmail.com>
2026-07-27 11:13:01 +0500

    Static List vs Live List

---
+++ file132.js
@@ -0,0 +1,7 @@
+// Static List vs Live List
+
+const staticList = document.querySelectorAll(".item");
+const liveList = document.getElementsByClassName("item");
+
+console.log(staticList);
+console.log(liveList);
